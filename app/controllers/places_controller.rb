a9eb9350b6d79259a55c5ee091b37a4cce59ec2b
Sean Connolly <sean.connolly@northwestern.edu>
2024-02-19 22:44:56 +0000

    Updated commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,14 @@
 class PlacesController < ApplicationController
 
 def index
-  # Find all the places
+  # Find all the places (index.html.erb in views)
   @places = Place.all
   # Render places in the index view
 end
   
 def show
-  # Find a place
-  @places = Place.find_by({"id" => params["id"]})
+  # Find a place (show.html.erb in views)
+  @place = Place.find_by({"id" => params["id"]})
 end
+
+

5b07e4db6729015f9fd1af84242ba0057a5da03e
Sean Connolly <sean.connolly@northwestern.edu>
2024-02-21 07:14:52 +0000

    Beginning Places

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,27 @@
 class EntriesController < ApplicationController
 
   def index
-    #render :template => "entries/index"
+    @entries = Entry.all
   end
+
+  def new
+    @entry = Entry.new
+  end
+
+#    # Start with a new place
+#    @entry = Entry.new
+
+#    # This assigns user-entered form data to the table of record, in our case places
+#    @entry["title"] = params["title"]
+#    @entry["description"] = params["description"]
+#    @entry["posted_on"] = params["posted_on"]
+#    @entry["place_id"] = params["place_id"]
+ 
+#    # This saves the new entry in the Place row
+#    @entry.save
+ 
+#    # Then we need to redirect the user
+#    redirect_to "/entries"
+#  end
 
 end

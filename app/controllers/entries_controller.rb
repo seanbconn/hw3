9134439a42a40c5accd6c18bb50175ff4ae38803
Sean Connolly <sean.connolly@northwestern.edu>
2024-02-28 05:07:50 +0000

    Will submit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -15,7 +15,6 @@ class EntriesController < ApplicationController
    @entry["title"] = params["title"]
    @entry["description"] = params["description"]
    @entry["posted_on"] = params["posted_on"]
-
    @entry["place_id"] = params["place_id"]
  
    @entry.save
